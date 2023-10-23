7fa49bd4320c14cda8cc98e5dfc1b626be1bc49b
Ivan <gremvanek@gmail.com>
2023-10-23 23:15:43 +0500

    Changes for function mask_card

--- src/masks.py
+++ src/masks.py
@@ -1,13 +1,20 @@
-def mask_card(card_mask: str) -> str:
-    """Возвращаем результат в необходимом виде XXXX XX** **** XXXX
+def mask_card(card_mask: str, card_name: str, card_names: list) -> str:
+    """Возвращаем результат в необходимом виде - Название карты и
+    замаскированный номер
+    XXXX XX** **** XXXX
     :param card_mask
     : Номер для маскирования
     :return: Маскированный по правилу номер
     """
-    if len(card_mask.replace(" ", "")) != 16:
-        return "Введенные данные неверные"
-    else:
-        return card_mask[0:4] + " " + card_mask[4:6] + "** **** " + card_mask[-4:]
+    for _ in card_names:
+        if card_name not in card_names:
+            print('Тип карты неизвестен')
+            quit()
+        else:
+            if len(card_mask.replace(" ", "")) != 16:
+                return "Введенные данные неверные"
+            else:
+                return card_name + ' ' + card_mask[0:4] + " " + card_mask[4:6] + "** **** " + card_mask[-4:]
 
 
 def mask_check(check_mask: str) -> str:
