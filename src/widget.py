14a4bf3bd44903c3d75b916a70a4947327a964f6
Ivan <gremvanek@gmail.com>
2023-10-23 22:57:45 +0500

    Add new parameters date, new_date

--- src/widget.py
+++ src/widget.py
@@ -1,2 +1,6 @@
 def date_optimizer(date: str) -> str:
+    """
+    Для выборки нужных символов из строки с датой
+    :param date: str :return: str
+    """
     return date[8: 10] + '.' + date[5: 7] + '.' + date[0: 4]
