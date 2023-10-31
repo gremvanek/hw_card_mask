5ff8aa9ad3892a249eebef876e9b88162ef63a3c
Ivan <gremvanek@gmail.com>
2023-10-31 18:43:58 +0500

    Added test_list_date_sort for reverse True and False. 84% tested

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -1,12 +1,12 @@
 import pytest
 
 import main
-from src.processing import check_email, list_dict_sort, sum_divisible_by_3_or_5
+from src.processing import check_email, list_date_sort, list_dict_sort, sum_divisible_by_3_or_5
 
 
 @pytest.fixture
 def state_list_dict_test() -> (
-        list[list[dict[str, str | int] | dict[str, str | int] | dict[str, str | int] | dict[str, str | int]]]
+    list[list[dict[str, str | int] | dict[str, str | int] | dict[str, str | int] | dict[str, str | int]]]
 ):
     return [
         [
@@ -26,11 +26,16 @@ def email_test() -> list[str]:
     "list_date_sort_test, expected",
     [
         (
-                main.state_list_dict,
-                [
-                    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-                    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-                ],
+            [
+                {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+                {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+                {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+                {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+            ],
+            [
+                {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+                {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+            ],
         )
     ],
 )
@@ -40,41 +45,44 @@ def test_list_dict_sort(list_date_sort_test: list[dict], expected: list[dict]) -
 
 @pytest.mark.parametrize(
     "state_list_dict_test, expected",
-    [(main.state_list_dict, [{'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-                             {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}])],
+    [
+        (
+            main.state_list_dict,
+            [
+                {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+                {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+            ],
+        )
+    ],
 )
 def test_list_dict_sort_2(state_list_dict_test: list[dict], expected: str) -> None:
     assert list_dict_sort(state_list_dict_test, "CANCELED") == expected
-
-
-@pytest.fixture
-def list_date_sort_test() -> (
-        list[list[dict[str, str | int] | dict[str, str | int] | dict[str, str | int] | dict[str, str | int]]]
-):
-    return [
-        [
-            {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-            {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-            {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-            {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-        ]
-    ]
 
 
 @pytest.mark.parametrize(
     "list_date_sort_test, expected",
     [
         (
-                list_date_sort_test,
-                [
-                    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-                    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-                    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-                    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-                ],
+            [
+                {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+                {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+                {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+                {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+            ],
+            [
+                {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+                {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+                {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+                {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+            ],
         )
     ],
 )
+def test_list_date_sort(list_date_sort_test: list[dict], expected: list[dict]) -> None:
+    assert list_date_sort(list_date_sort_test, True) == expected
+    assert list_date_sort(list_date_sort_test, False) == expected[::-1]
+
+
 @pytest.fixture
 def lst_test() -> list[list[int]]:
     return [[0], [1, 4, 7], [1, 2, 3, 4, 5]]
