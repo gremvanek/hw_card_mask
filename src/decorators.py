3b394320302d26707107043f909087a0d41ed4e3
Ivan <gremvanek@gmail.com>
2023-11-07 20:29:39 +0500

    Сделал наконец декоратор для функции.

--- src/decorators.py
+++ src/decorators.py
@@ -1,42 +1,28 @@
 from datetime import datetime
-from typing import Any
+from functools import wraps
 
 
-def log(func: Any) -> Any:
-    """
-    Декоратор для записи результатов выполнения функций с временем выполнения
-    :param func: Any
-    :return: Any
-    """
+def log(file_name=None):
+    def wrapper(func):
+        @wraps(func)
+        def inner(*args):
+            result = func(*args)
+            current_datetime = datetime.now().strftime("%Y-%m-%d %H-%M-%S")
+            str_current_datetime = str(current_datetime)
+            if file_name is not None:
+                with open(file_name, "w", encoding="utf-8") as f:
+                    f.write(f"{str_current_datetime} Function called {func.__name__} with args: {args} "
+                            f". Result: {result}\n")
+            else:
+                print(f"{str_current_datetime} Function called {func.__name__} with args: {args} "
+                      f". Result: {result}\n")
 
-    def wrapper(*args: Any, **kwargs: Any) -> Any:
-        """
-        Функция для печати результата в консоль или файл
-        :param filename: Any
-        :param args: Any
-        :param kwargs: Any
-        :return: Any
-        """
-        current_datetime = datetime.now().strftime("%Y-%m-%d %H-%M-%S")
-        str_current_datetime = str(current_datetime)
-        result = func(*args, **kwargs)
-        filename = 'my_log.txt'
-        if not filename:
-            print(
-                f"{str_current_datetime} Function called {func.__name__} with args: {args}"
-                f"and kwargs: {kwargs}. Result: {result}"
-            )
-        else:
-            with open(filename, "w", encoding="utf-8") as f:
-                f.write(
-                    f"{str_current_datetime} Function called {func.__name__} with args: {args}"
-                    f"and kwargs: {kwargs}. Result: {result}\n"
-                )
+        return inner
 
     return wrapper
 
 
-@log
+@log()
 def my_function(x: int, y: int) -> int:
     """
     Функция из дз
